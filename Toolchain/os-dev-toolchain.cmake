06857b40616176dd2e4317471ac450bb0c59bce7
Jared <jared.j.barnak@gmail.com>
2022-12-27 12:04:01 -0500

    CMake: Add languages so that I can properly identify compilers

--- Toolchain/os-dev-toolchain.cmake
+++ Toolchain/os-dev-toolchain.cmake
@@ -1,5 +1,4 @@
 set(CMAKE_C_COMPILER_WORKS 1)   # Skip Test for Cross Compilation
 set(CMAKE_CXX_COMPILER_WORKS 1) # Skip Test for Cross Compilation
 set(CMAKE_SYSTEM_PROCESSOR i686)
-set(CMAKE_C_COMPILER ${CMAKE_SOURCE_DIR}/Toolchain/opt/cross/bin/i686-elf-gcc)
 set(CMAKE_CXX_COMPILER ${CMAKE_SOURCE_DIR}/Toolchain/opt/cross/bin/i686-elf-g++)
