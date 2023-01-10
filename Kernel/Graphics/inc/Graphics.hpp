c33963af82750d2373bc98838371cdc146d9f953
Jared <jared.j.barnak@gmail.com>
2023-01-09 21:51:19 -0500

    CMake + Testing: libstd++

--- Kernel/Graphics/inc/Graphics.hpp
+++ Kernel/Graphics/inc/Graphics.hpp
@@ -3,34 +3,38 @@
 // Only a limited amount of headers are allowed
 // becasuse we are in a Freestanding environment
 // https://en.cppreference.com/w/cpp/freestanding
+// #include <stdint.h>
 #include <cstdint>
+#include <cstddef>
 
 namespace Kernel {
 
 // VGA Text Mode
 // Allows the ability to write to the terminal
-class GraphicsDriver {
-public:
-  /* Hardware text mode color constants. */
-  enum class VGA_Color {
-    VGA_COLOR_BLACK,
-    VGA_COLOR_BLUE,
-    VGA_COLOR_GREEN,
-    VGA_COLOR_CYAN,
-    VGA_COLOR_RED,
-    VGA_COLOR_MAGENTA,
-    VGA_COLOR_BROWN,
-    VGA_COLOR_LIGHT_GREY,
-    VGA_COLOR_DARK_GREY,
-    VGA_COLOR_LIGHT_BLUE,
-    VGA_COLOR_LIGHT_GREEN,
-    VGA_COLOR_LIGHT_CYAN,
-    VGA_COLOR_LIGHT_RED,
-    VGA_COLOR_LIGHT_MAGENTA,
-    VGA_COLOR_LIGHT_BROWN,
-    VGA_COLOR_WHITE,
-  };
+class GraphicsDriver
+{
+  public:
+    /* Hardware text mode color constants. */
+    enum class VGA_Color {
+        VGA_COLOR_BLACK,
+        VGA_COLOR_BLUE,
+        VGA_COLOR_GREEN,
+        VGA_COLOR_CYAN,
+        VGA_COLOR_RED,
+        VGA_COLOR_MAGENTA,
+        VGA_COLOR_BROWN,
+        VGA_COLOR_LIGHT_GREY,
+        VGA_COLOR_DARK_GREY,
+        VGA_COLOR_LIGHT_BLUE,
+        VGA_COLOR_LIGHT_GREEN,
+        VGA_COLOR_LIGHT_CYAN,
+        VGA_COLOR_LIGHT_RED,
+        VGA_COLOR_LIGHT_MAGENTA,
+        VGA_COLOR_LIGHT_BROWN,
+        VGA_COLOR_WHITE,
+    };
 
-private:
+  private:
 };
-} // namespace Kernel
+
+}// namespace Kernel
--- Toolchain/os-dev-toolchain.cmake
+++ Toolchain/os-dev-toolchain.cmake
@@ -1,4 +1,6 @@
+set(CMAKE_SYSROOT ${CMAKE_SOURCE_DIR}/Toolchain/)
 set(CMAKE_C_COMPILER_WORKS 1)   # Skip Test because we are in a free-standing environment
 set(CMAKE_CXX_COMPILER_WORKS 1) # Skip Test because we are in a free-standing environment
 set(CMAKE_SYSTEM_PROCESSOR i686)
+set(CMAKE_CXX_COMPILER ${CMAKE_SOURCE_DIR}/Toolchain/opt/cross/bin/i686-elf-gcc)
 set(CMAKE_CXX_COMPILER ${CMAKE_SOURCE_DIR}/Toolchain/opt/cross/bin/i686-elf-g++)
