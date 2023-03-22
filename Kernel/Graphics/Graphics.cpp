e73a19225efec83865066dae52bde04289f9cd33
j-barnak <96548573+j-barnak@users.noreply.github.com>
2023-03-21 23:09:45 -0400

    Update Graphics.cpp

--- Kernel/Graphics/Graphics.cpp
+++ Kernel/Graphics/Graphics.cpp
@@ -8,7 +8,6 @@ Graphics::Graphics()
 
 void Graphics::init_terminal() 
 {
-    // Using 0 as a value because we are clearing the terminal
     auto initial_value = ScreenChar { static_cast<std::uint8_t>('a'), ColorCode { VGA_Color::White, VGA_Color::Black } };
 
     for (std::size_t i = 0; i < m_row; ++i) {
