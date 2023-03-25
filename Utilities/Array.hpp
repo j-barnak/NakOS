f85de44c2d60b6a8f583bd6c3f76753685d3084f
Jared B <jared.j.barnak@gmail.com>
2023-03-25 18:59:06 -0400

    Array: Added new class called Array to replace raw arrays
    I'm in the progress of porting members of the standard library. This
    should make everything easier and is a great learning exercise.

---
+++ Utilities/Array.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <memory>
+
+template<typename T, std::size_t N>
+struct Array
+{
+    T __array[N];
+    T& operator[](std::ptrdiff_t index);
+    Array() = delete;
+    T* begin();
+    T* end();
+    constexpr T* cbegin();
+    constexpr T* cend();
+};
+
+
+template<typename T, std::size_t N>
+T& Array<T, N>::operator[](std::ptrdiff_t index)
+{
+    return __array[index];
+}
+
+template<typename T, std::size_t N>
+T* Array<T, N>::begin()
+{
+    return &__array[0];
+}
+
+template<typename T, std::size_t N>
+T* Array<T, N>::end()
+{
+    return &__array[N];
+}
+
+template<typename T, std::size_t N>
+constexpr T* Array<T, N>::cbegin()
+{
+    return &__array[0];
+}
+
+template<typename T, std::size_t N>
+constexpr T* Array<T, N>::cend()
+{
+    return &__array[N];
+}
