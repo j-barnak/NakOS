bc61a2a37798046ec687ecd902d7974b7ac88b13
Jared Barnak <karnrabdev@gmail.com>
2025-09-14 22:24:16 -0400

    Small update

--- Arch/x86/Processor/Descriptors.hpp
+++ Arch/x86/Processor/Descriptors.hpp
@@ -3,6 +3,19 @@
 #include <cstdint>
 #include <memory>
 
+inline void crash()
+{
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wall"
+#pragma GCC diagnostic ignored "-Wextra"
+#pragma GCC diagnostic ignored "-Wpedantic"
+#pragma GCC diagnostic ignored "-Wundef"
+    int *__CRASH_PTR = nullptr;
+    *__CRASH_PTR;
+#pragma GCC diagnostic pop
+}
+
+
 namespace Processor {
 
 template<std::uint8_t Size>
@@ -17,9 +30,15 @@ class Descriptors
 
     void load_descriptor_entry(const Entry &entry, std::ptrdiff_t index);
     void load_gdtr();
+
+    static void set_base(const std::uint32_t base, Descriptors<Size>::Entry &descriptor);
+    static void set_flags(const std::uint8_t flags, Descriptors<Size>::Entry &descriptor);
+    static void set_access_byte(const std::uint8_t access_byte, Descriptors<Size>::Entry &descriptor);
+
     // clang-format off
     constexpr std::uint8_t amount_of_entries() const { return Size; }
-    
+    // clang-format on
+
     struct [[gnu::packed]] Entry
     {
         std::uint16_t segment_limit_low;
@@ -32,7 +51,7 @@ class Descriptors
         std::uint8_t segment_limit_high : 4;
         std::uint8_t available : 1;
         std::uint8_t reserved : 1;
-        std::uint8_t d_or_b : 1;
+        std::uint8_t db : 1;
         std::uint8_t granularity : 1;
         std::uint8_t base_address_high;
     };
@@ -68,4 +87,61 @@ void Descriptors<Size>::load_descriptor_entry(const Descriptors<Size>::Entry &en
     m_entries[index] = entry;
 }
 
-}// namespace Processor
+template<std::uint8_t Size>
+void Descriptors<Size>::set_base(const std::uint32_t base, Descriptors<Size>::Entry &entry)
+{
+    const std::uint32_t lower_mask = 0x0000FFFF;
+
+    const std::uint32_t middle_mask = 0x00FF0000;
+    const std::uint8_t middle_shift = 16;
+
+    const std::uint32_t high_mask = 0xFF000000;
+    const std::uint8_t high_shift = 24;
+
+    entry.base_address_low = (lower_mask & base);
+    entry.base_address_mid = (middle_mask & base) >> middle_shift;
+    entry.base_address_high = (high_mask & base) >> high_shift;
+}
+
+// Flags:
+//     +---+---+---+---+
+//     | 1 | 1 | 1 | 1 |
+//     +---+---+---+---+
+//     | G | DB| L | R |
+//     +---+---+---+---+
+template<std::uint8_t Size>
+void Descriptors<Size>::set_flags(std::uint8_t flags, Descriptors<Size>::Entry &entry)
+{
+    [[unlikely]] if (flags > 0b1111) {
+        crash();
+    }
+
+    const std::uint8_t g_mask = 0b1000;
+    const std::uint8_t g_shift = 3;
+
+    const std::uint8_t db_mask = 0b0100;
+    const std::uint8_t db_shift = 2;
+
+    const std::uint8_t l_mask = 0b0010;
+    const std::uint8_t l_shift = 1;
+
+    const std::uint8_t r_mask = 0b0000;
+    const std::uint8_t r_shift = 0;
+
+    entry.granularity = (g_mask & flags) > g_shift;
+    entry.db = (db_mask & flags) > db_shift;
+    entry.l_mask = (l_mask & flags) > db_shift;
+    entry.reserved = r_mask & flags;
+}
+
+// Access Byte:
+//     +---+----+---+---+---+---+---+
+//     | 1 | 02 | 1 | 1 | 1 | 1 | 1 |
+//     +---+----+---+---+---+---+---+
+//     | P | DPL| S | E | DC| RW| A |
+//     +---+----+---+---+---+---+---+
+template<std::uint8_t Size>
+void Descriptors<Size>::set_access_byte(std::uint8_t access_byte, Descriptors<Size>::Entry &entry)
+{}
+
+} // namespace Processor
--- Arch/x86/Processor/x86_init.cpp
+++ Arch/x86/Processor/x86_init.cpp
@@ -9,15 +9,28 @@ auto tss = Processor::Descriptors<gdt.amount_of_entries()>::Entry {};
 
 static void load_gdt_entries()
 {
-    // TODO:  Entry 0 in a descriptor table, or the Null Descriptor, is never referenced by the processor, and should
-    //        always contain no data. Certain emulators, like Bochs, will complain about limit exceptions if you do not
-    //        have one present. Some use this descriptor to store a pointer to the GDT itself (to use with the LGDT
-    //        instruction). The null descriptor is 8 bytes wide and the pointer is 6 bytes wide so it might just be the
-    //        perfect place for this.
-    //        A DPL 0 Code Segment descriptor (for your kernel)
-    //        A Data Segment descriptor (writing to code segments is not allowed)
-    //        A Task State Segment segment descriptor (its very useful to have at least one)
-    //        Room for more segments if you need them (e.g. user-level, LDTs, more TSS, whatever)
+    // Links: - https://wiki.osdev.org/GDT_Tutorial
+    //        - https://wiki.osdev.org/Global_Descriptor_Table
+
+    // The following Segments for paging _and_ ignoring segmentation. Segmentation in long mode is ignored; and so it
+    // makes doesn't make sense to account use segmentation.
+    // - NULL descriptor
+    // - Kernel Mode Code Segment
+    // - Kernel Mode Data Segment
+    //   - Base:        0x0000
+    //   - Limit:       0xFFFF
+    //   - Access Byte: 0x92
+    //   - Flags:       0xC
+    // - User Mode Code Segment
+    //   - Base:        0x0000
+    //   - Limit:       0xFFFF
+    //   - Access Byte: 0xFA
+    //   - Flags:       0xC
+    // - User Mode Data Segment
+    //   - Base:        std::addrof(tss)
+    //   - Limit:       sizeof(tss) - 1
+    //   - Access Byte: 0x89
+    //   - Flags:       0x00
 
     constexpr auto size = gdt.amount_of_entries();
 
@@ -26,90 +39,34 @@ static void load_gdt_entries()
     //       - DPL represents rings (0-3 with 0 being the most privileged)
     auto null_entry = Processor::Descriptors<size>::Entry {};
 
+    //   - Base:        0x0000
+    //   - Limit:       0xFFFF
+    //   - Access Byte: 0x9A
+    //     - 1 | 00 | 1 | 1 | 0 | 1 | 0
+    //       P | DPL| S | E | DC| RW| A
+    //   - Flags:       0xC
+    //     - | 1 | 1 | 0 | 0 |
+    //       | G | DB| L | R |
     auto kernel_mode_code_segment = Processor::Descriptors<size>::Entry {
-        .segment_limit_low = 0,
-        .base_address_low = 0,
-        .base_address_mid = 0,
-        .type = 10,
-        .system = 1,
-        .descriptor_privilege_level = 0,
-        .present = 1,
-        .segment_limit_high = 0,
-        .available = 1,
-        .reserved = 0,
-        .d_or_b = 1,
-        .granularity = 1,
-        .base_address_high = 0,
+        .segment_limit_low = std::uint16_t { 0xFFFF },
+        .base_address_low = std::uint16_t { 0x0000 },
+        .base_address_mid = std::uint8_t { 0x00 },
+        .type = std::uint8_t { 0x1 }, // TODO Need to verify
+        .system = std::uint8_t { 0x1 },
+        .descriptor_privilege_level = std::uint8_t { 0x0 },
+        .present = std::uint8_t { 0x0 },
+        .segment_limit_high = std::uint8_t { 0xF },
+        .available = std::uint8_t { 0x1 }, // TODO Need to verify
+        .reserved = std::uint8_t { 0x0 },
+        .db = std::uint8_t { 0x0 },
+        .granularity = std::uint8_t { 0x1 },
+        .base_address_high = std::uint8_t { 0xFF },
     };
 
-    auto kernel_mode_data_segment = Processor::Descriptors<size>::Entry {
-        .segment_limit_low = 0,
-        .base_address_low = 0,
-        .base_address_mid = 0,
-        .type = 6,
-        .system = 1,
-        .descriptor_privilege_level = 0,
-        .present = 1,
-        .segment_limit_high = 0,
-        .available = 1,
-        .reserved = 0,
-        .d_or_b = 0,
-        .granularity = 0,
-        .base_address_high = 0,
-    };
+    auto kernel_mode_data_segment { 1 };
 
-    auto user_mode_code_segment = Processor::Descriptors<size>::Entry {
-        .segment_limit_low = 0,
-        .base_address_low = 0,
-        .base_address_mid = 0,
-        .type = 10,
-        .system = 1,
-        .descriptor_privilege_level = 3,
-        .present = 1,
-        .segment_limit_high = 0,
-        .available = 1,
-        .reserved = 0,
-        .d_or_b = 1,
-        .granularity = 1,
-        .base_address_high = 0,
-    };
-
-    auto user_mode_data_segment = Processor::Descriptors<size>::Entry {
-        .segment_limit_low = 0,
-        .base_address_low = 0,
-        .base_address_mid = 0,
-        .type = 6,
-        .system = 1,
-        .descriptor_privilege_level = 3,
-        .present = 1,
-        .segment_limit_high = 0,
-        .available = 1,
-        .reserved = 0,
-        .d_or_b = 0,
-        .granularity = 0,
-        .base_address_high = 0,
-    };
-
-    auto task_state_segment = Processor::Descriptors<size>::Entry {
-        .segment_limit_low = 0,
-        .base_address_low = 0,
-        .base_address_mid = 0,
-        .type = 2,
-        .system = 1,
-        .descriptor_privilege_level = 0,
-        .present = 1,
-        .segment_limit_high = 0,
-        .available = 1,
-        .reserved = 0,
-        .d_or_b = 0,
-        .granularity = 0,
-        .base_address_high = 0,
-    };
 
     gdt.load_descriptor_entry(null_entry, 0);
-    gdt.load_descriptor_entry(kernel_mode_code_segment, 1);
-    gdt.load_descriptor_entry(kernel_mode_data_segment, 2);
-    gdt.load_descriptor_entry(task_state_segment, 3);
 }
 
 void gdt_init()
