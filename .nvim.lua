c5f6d7a14bbeddc7284f325103ef3ca9a5ccb9c4
Jared Barnak <jared@invalid.invalid>
2026-01-19 22:22:26 -0500

    update nvim.lua

--- .nvim.lua
+++ .nvim.lua
@@ -1,14 +1,13 @@
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = false
 
-require("lspconfig").clangd.setup({
+vim.lsp.config("clangd", {
   capabilities = capabilities,
-	on_new_config = function(config)
-		config.cmd = {
-			"clangd",
-			"--background-index",
-			"--query-driver=/home/jared/Projects/NakOS/Toolchain/opt/cross/bin/i686-elf-g++",
-		}
-	end,
+  cmd = {
+    "clangd",
+    "--background-index",
+    "--query-driver=/home/jared/Projects/NakOS/Toolchain/opt/cross/bin/i686-elf-g++",
+  },
 })
+
+vim.lsp.enable("clangd")
